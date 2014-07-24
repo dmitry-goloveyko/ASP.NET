ff692a4f062dfef9a283bc49ffb408f0978616c0
dmitry-goloveyko <d.goloveyko@gmail.com>
2014-07-24 14:22:37 +0300

    Realised Cart

---
+++ FoodOrder/FoodOrder.Domain/Entities/Cart.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Domain.Entities
+{
+	public class Cart
+	{
+		private List<CartLine> lineCollection = new List<CartLine>();
+
+		public void AddItem(Food food, int quantity)
+		{
+			CartLine line = lineCollection
+			  .Where(p => p.Food.FoodID == food.FoodID)
+			  .FirstOrDefault();
+
+			if (line == null)
+			{
+				lineCollection.Add(new CartLine
+				{
+					Food = food,
+					Quantity = quantity
+				});
+			}
+			else
+			{
+				line.Quantity += quantity;
+			}
+		}
+
+		public void RemoveLine(Food food)
+		{
+			lineCollection.RemoveAll(l => l.Food.FoodID == food.FoodID);
+		}
+
+		public decimal ComputeTotalValue()
+		{
+			return lineCollection.Sum(e => e.Food.Price * e.Quantity);
+		}
+
+		public void Clear()
+		{
+			lineCollection.Clear();
+		}
+
+		public IEnumerable<CartLine> Lines
+		{
+			get { return lineCollection; }
+		}
+	}
+
+	public class CartLine
+	{
+		public Food Food { get; set; }
+		public int Quantity { get; set; }
+	}
+
+}
---
+++ FoodOrder/FoodOrder.WebUI/Controllers/CartController.cs
@@ -0,0 +1,63 @@
+﻿using FoodOrder.Domain.Abstract;
+using FoodOrder.Domain.Entities;
+using FoodOrder.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodOrder.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+		private IFoodRepository repository;
+
+		public CartController(IFoodRepository repo)
+		{
+			repository = repo;
+		}
+
+		public ViewResult Index(string returnUrl)
+		{
+			return View(new CartIndexViewModel
+			{
+				Cart = GetCart(),
+				ReturnUrl = returnUrl
+			});
+		}
+
+		public RedirectToRouteResult AddToCart(int foodId, string returnUrl)
+		{
+			Food food = repository.Foods
+			.FirstOrDefault(f => f.FoodID == foodId);
+			if (food != null)
+			{
+				GetCart().AddItem(food, 1);
+			}
+			return RedirectToAction("Index", new { returnUrl });
+		}
+
+		public RedirectToRouteResult RemoveFromCart(int foodId, string returnUrl)
+		{
+			Food food = repository.Foods
+			.FirstOrDefault(f => f.FoodID == foodId);
+			if (food != null)
+			{
+				GetCart().RemoveLine(food);
+			}
+			return RedirectToAction("Index", new { returnUrl });
+		}
+
+		private Cart GetCart()
+		{
+			Cart cart = (Cart)Session["Cart"];
+			if (cart == null)
+			{
+				cart = new Cart();
+				Session["Cart"] = cart;
+			}
+			return cart;
+		}
+    }
+}
